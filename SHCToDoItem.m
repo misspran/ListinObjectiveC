d585eb458c05f9e28c0322ad8330ab53c61620d4
Vi Tran <vi@startupgrind.com>
2015-01-05 19:44:52 -0800

    tableView

---
+++ SHCToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  SHCToDoItem.m
+//  ListinObjective-C
+//
+//  Created by Vi Tran on 12/31/14.
+//  Copyright (c) 2014 Vi Tran. All rights reserved.
+//
+
+#import "SHCToDoItem.h"
+
+@implementation SHCToDoItem
+
+@end
